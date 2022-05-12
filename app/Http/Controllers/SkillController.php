b0eddf340112965753798a11306ab214fb0c3a20
João Cabral Périgo <joperigo@gmail.com>
2022-05-12 01:29:46 -0300

    Create new skill

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Skill;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Inertia\Inertia;
 
 class SkillController extends Controller
@@ -12,7 +13,27 @@ class SkillController extends Controller
     {
         return Inertia::render('Skills/All', [
             'skills' => Skill::all(),
+            'availableColors' => Skill::getAvailableBackgroundColors(),
         ]);
     }
-    
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => [
+                'required',
+                'max:255',
+                Rule::unique(Skill::class)
+            ],
+            'color' => [
+                'required',
+                'in:' . implode(',', Skill::getAvailableBackgroundColors())
+            ],
+        ]);
+
+        Skill::create($request->all());
+
+        return redirect()->route('skills.index');
+    }
+
 }
